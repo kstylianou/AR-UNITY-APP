ff3d39d866b5696f8055e6dca08cb8d2bfbe9f6a
kstylianou <stylianou98@gmail.com>
2021-03-26 10:42:15 +0200

    Load

---
+++ AR/Assets/PUZZLE.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PUZZLE : MonoBehaviour
+{
+    public GameObject p_1;
+    public GameObject p_2;
+    public GameObject p_3;
+    public GameObject p_4;
+
+    void Update()
+    {
+        float p1_Y = p_1.transform.rotation.eulerAngles.y;
+        float p2_Y = p_2.transform.rotation.eulerAngles.y;
+        float p3_Y = p_3.transform.rotation.eulerAngles.y;
+        float p4_Y = p_4.transform.rotation.eulerAngles.y;
+
+        int a = (int)p1_Y;
+        int b = (int)p2_Y;
+
+        if (p1_Y == p2_Y)
+        {
+            SceneManager.LoadScene("Main");
+        }
+    }
+
+}
---
+++ AR/Assets/collision_p1_2.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class collision_p1_2 : MonoBehaviour
+{
+  void OnTriggerEnter(Collider col)
+    {
+        SceneManager.LoadScene("Main");
+
+        if (col.GetComponent<Collider>().name == "puzzle_p2_1")
+        {
+            SceneManager.LoadScene("Main");
+        }
+    }
+}
