925db997f9d4d2aa3e774b2c15f761faae036150
Kyriakos Stylianou <info@finditincyprus.com>
2021-03-26 15:38:06 +0200

    test

--- AR/Assets/collision_p1_2.cs
+++ AR/Assets/collision_p1_2.cs
@@ -5,15 +5,30 @@ using UnityEngine.SceneManagement;
 
 public class collision_p1_2 : MonoBehaviour
 {
+    bool snapped = false;
+    GameObject snapparent; // the gameobject this transform will be snapped to
+    Vector3 offset; // the offset of this object's position from the parent
+
+    void Update()
+    {
+        if (snapped == true)
+        {
+            //retain this objects position in relation to the parent
+            transform.position = this.transform.position + offset;
+        }
+    }
+
     void OnTriggerEnter(Collider col)
     {
-        //SceneManager.LoadScene("Main");
+        if (col.gameObject.name == "P_2_Collision")
+        {
+            //If the GameObject's name matches the one you suggest, output this message in the console
+            //SceneManager.LoadScene("Main");
 
-        //if (col.GetComponent<Collider>().name == "puzzle_p2_1")
-        //{
-        //    SceneManager.LoadScene("Main");
+            snapped = true;
+            snapparent = col.gameObject;
+            offset = transform.position - snapparent.transform.position; //store relation to parent
 
-        Debug.Log("Trigger");
+        }
     }
-    
 }
