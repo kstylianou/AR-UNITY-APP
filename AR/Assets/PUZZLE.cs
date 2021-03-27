5a60dbdca79ed8b5288ad81f84f70523c9506e11
Kyriakos Stylianou <stylianou98@gmail.com>
2021-03-27 14:22:38 +0200

    test

--- AR/Assets/PUZZLE.cs
+++ AR/Assets/PUZZLE.cs
@@ -8,9 +8,10 @@ public class PUZZLE : MonoBehaviour
 
     void Update()
     {
-        Debug.Log(collision_p1_2.snapped);
+        //Debug.Log("P1 " + collision_p1_2.snapped);
+        //Debug.Log("P3 " + collision_p1_3.snapped);
 
-        if(collision_p1_2.snapped == true && collision_p1_3.snapped == true)
+        if (collision_p1_2.snapped == true && collision_p1_3.snapped == true && collision_p2_4.snapped == true && collision_p3_4.snapped == true)
             SceneManager.LoadScene("Main");
             //Debug.Log("Trigger");
 
---
+++ AR/Assets/collision_p2_4.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collision_p2_4 : MonoBehaviour
+{
+    public static bool snapped = false;
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.name == "P_4_2_Collision")
+        {
+            snapped = true;
+        }
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        if (col.gameObject.name == "P_4_2_Collision")
+        {
+            snapped = false;
+        }
+    }
+}
